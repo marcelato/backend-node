8451a0270c0f94af9fd07377056e41a7841d161a
rolandoto <rolando22_@outlook.com>
2024-12-03 14:42:48 -0800

    initial commit

--- Routes/User.route.js
+++ Routes/User.route.js
@@ -2,7 +2,7 @@ const router =require('express').Router()
 const { ValidarCampos } = require('../middleweres/middleweres');
 const { check } = require('express-validator');
 const { CreateUsuario, LoginUser } = require('../controller/admin/user');
-
+//dsadas
 router.post("/register",[
     check("username","es obligatorio").not().isEmpty(),
     check("password","es obligatorio").not().isEmpty(),
